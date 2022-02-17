affecc9c3563bdc83579f5aa403f64383cd09143
ELECFREAKS.Admin <support@elecfreaks.com>
2022-02-17 17:02:26 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+//% color="	#8600FF" weight=100
+//% block="makecode_test"
+namespace makecode_test {
+    //% block="Send Hex data %data_hex"
+    export function my_serial_hex(data_hex:number) {
+        serial.writeNumber(data_hex)
+    }
+}
